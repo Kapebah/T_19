344f69a2945d04b12228f4c33b3057b8864038cd
Kapebah <sh1n0sh1@mail.ru>
2024-04-08 08:55:33 +0300

    example_1

---
+++ T19_1/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace T19_1
+{
+    class MyClass : IMyInterface
+    {
+        public void Show()
+        {
+            WriteLine("MyClass() is implemented\n//MyClass() - реализован");
+        }
+    }
+}
---
+++ T19_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace T19_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MyClass myClassOb = new MyClass();
+                myClassOb.Show();
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Исключение: {ex.Message}");
+                WriteLine($"Метод: {ex.TargetSite}");
+                WriteLine($"Трассировка стека: {ex.StackTrace}");
+            }
+            ReadKey();
+        }
+    }
+}
